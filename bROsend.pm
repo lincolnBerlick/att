4ee873662bf65b7bc5b7cae5bf351835b08cfe4e
lincolnBerlick <lincoln.berlick@gmail.com>
2018-12-11 10:34:39 -0200

    Update bROsend.pm

--- bROsend.pm
+++ bROsend.pm
@@ -23,7 +23,7 @@ sub new {
 		'083C' => ['skill_use', 'v2 a4', [qw(lv skillID targetID)]],
 		'0437' => ['character_move','a3', [qw(coords)]],
 		'035F' => ['sync', 'V', [qw(time)]],
-		'0202' => ['actor_look_at', 'v C', [qw(head body)]],
+		'093B' => ['actor_look_at', 'v C', [qw(head body)]],
 		'07E4' => ['item_take', 'a4', [qw(ID)]],
 		'0362' => ['item_drop', 'v2', [qw(index amount)]],
 		'07EC' => ['storage_item_add', 'v V', [qw(index amount)]],
@@ -36,11 +36,11 @@ sub new {
 		'0815' => ['buy_bulk_openShop', 'a4 c a*', [qw(limitZeny result itemInfo)]], # Selling Store
 		'0365' => ['booking_register', 'v8', [qw(level MapID job0 job1 job2 job3 job4 job5)]], # Booking Register
 		'0281' => ['item_list_res', 'v V2 a*', [qw(len type action itemInfo)]],
-		'022D' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
+		'088F' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
 		'0802' => ['party_join_request_by_name', 'Z24', [qw(partyName)]],
-		'023B' => ['friend_request', 'a*', [qw(username)]],
-		'0361' => ['homunculus_command', 'v C', [qw(commandType, commandID)]],
-		'094E' => ['storage_password'],		
+		'08A4' => ['friend_request', 'a*', [qw(username)]],
+		'094B' => ['homunculus_command', 'v C', [qw(commandType, commandID)]],
+		'088D' => ['storage_password'],		
 	);
 	
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
@@ -54,7 +54,7 @@ sub new {
 	
 	while (my ($k, $v) = each %packets) { $handlers{$v->[0]} = $k}
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
-	$self->cryptKeys(1482188073, 77405341, 222130077);
+	$self->cryptKeys(1706961401, 13967641, 96734588);
 
 	return $self;
 }
