2dfaa106cb6132be9537cabb86092de24563fabc
lincolnBerlick <lincoln.berlick@gmail.com>
2019-04-02 17:23:37 -0300

    Update bROsend.pm

--- bROsend.pm
+++ bROsend.pm
@@ -23,7 +23,7 @@ sub new {
 		'083C' => ['skill_use', 'v2 a4', [qw(lv skillID targetID)]],
 		'0437' => ['character_move','a3', [qw(coords)]],
 		'035F' => ['sync', 'V', [qw(time)]],
-		'0817' => ['actor_look_at', 'v C', [qw(head body)]],
+		'0940' => ['actor_look_at', 'v C', [qw(head body)]],
 		'07E4' => ['item_take', 'a4', [qw(ID)]],
 		'0362' => ['item_drop', 'v2', [qw(index amount)]],
 		'07EC' => ['storage_item_add', 'v V', [qw(index amount)]],
@@ -32,15 +32,15 @@ sub new {
 		'096A' => ['actor_info_request', 'a4', [qw(ID)]],
 		'0368' => ['actor_name_request', 'a4', [qw(ID)]],	
 		'0811' => ['buy_bulk_buyer', 'a4 a4 a*', [qw(buyerID buyingStoreID itemInfo)]], # Buying Store
-		'0361' => ['buy_bulk_closeShop'],
+		'0202' => ['buy_bulk_closeShop'],
 		'0815' => ['buy_bulk_openShop', 'a4 c a*', [qw(limitZeny result itemInfo)]], # Selling Store
 		'0365' => ['booking_register', 'v8', [qw(level MapID job0 job1 job2 job3 job4 job5)]], # Booking Register
 		'0281' => ['item_list_res', 'v V2 a*', [qw(len type action itemInfo)]],
-		'0939' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
+		'0899' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
 		'0802' => ['party_join_request_by_name', 'Z24', [qw(partyName)]],
-		'085B' => ['friend_request', 'a*', [qw(username)]],
-		'0835' => ['homunculus_command', 'v C', [qw(commandType, commandID)]],
-		'0954' => ['storage_password'],		
+		'0817' => ['friend_request', 'a*', [qw(username)]],
+		'08A1' => ['homunculus_command', 'v C', [qw(commandType, commandID)]],
+		'085C' => ['storage_password'],		
 	);
 	
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
@@ -54,7 +54,7 @@ sub new {
 	
 	while (my ($k, $v) = each %packets) { $handlers{$v->[0]} = $k}
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
-	$self->cryptKeys(2090695261, 1724789286, 1620518720);
+	$self->cryptKeys(1797718761, 1438149660, 2009821891);
 
 	return $self;
 }
